91201260f0e6a583348e46a1b5b47d980286bf90
Josh Suereth <Joshua.Suereth@gmail.com>
2009-10-18 22:23:26 -0400

    Added and to ManagedResource trait.  General cleanup of API

--- src/main/scala/scala/resource/ManagedResource.scala
+++ src/main/scala/scala/resource/ManagedResource.scala
@@ -16,8 +16,8 @@ package scala.resource
 import scala.collection.Traversable
 import scala.collection.Sequence
 import scala.collection.Iterator
-import scala.collection.Iterable
 import scala.util.control.Exception
+import scala.Either
 
 
 /**
@@ -28,7 +28,7 @@ trait ManagedResource[+R] {
   /**
    * This method is used to perform operations on a resource while the resource is open.
    */
-  def map[B](f : R => B) : TranslatedResource[B]
+  def map[B](f : R => B) : ExtractableManagedResource[B]
 
   /**
    * This method is used to immediately perform operations on a resource while it is open, ensuring the resource is
@@ -68,10 +68,18 @@ trait ManagedResource[+R] {
   def toTraversable[B](f : R => Iterator[B]) : Traversable[B]
 
   /**
-   * This method returns a clone of this object where all operations are defered until forced.
+   * Creates a new resource that is the aggregation of this resource and another.
+   *
+   * @param that
+   *          The other resource
+   */
+  def and[B](that : ManagedResource[B]) : ManagedResource[(R,B)]
+
+  /**
+   *  This method returns a clone of this object where all operations are defered until forced.
    *
    * @returns
-   *        A lazy version of the ManagedResource
+   * A lazy version of the ManagedResource
    */
   //def defer : ManagedResource[R,H]
 }
@@ -79,18 +87,18 @@ trait ManagedResource[+R] {
 /**
  * This trait represents a resource that has been modified (or will be modified) inside an ARM block of some kind.
  */
-trait TranslatedResource[+A] {
+trait ExtractableManagedResource[+A] {
 
   /**
    * This method is used to translate a resource further.
    */
-  def map[B](f : A => B) : TranslatedResource[B]
+  def map[B](f : A => B) : ExtractableManagedResource[B]
 
   /**
    * This method is used to immediately perform operations on a resource while it is open, ensuring the resource is
    * closed before returning.
    */
-  def flatMap[B](f : A => B) : TranslatedResource[B]    
+  def flatMap[B](f : A => B) : ExtractableManagedResource[B]
 
   /**
    * This method is used to immediately perform operations on a resource while it is open, ensuring the resource is
@@ -129,12 +137,12 @@ trait TranslatedResource[+A] {
 }
 
 /**
- * And implementation of a TranslatedResource that defers all processing until the user pulls out information using
+ * And implementation of a ExtractableManagedResource that defers all processing until the user pulls out information using
  * either or opt functions.
  */
-class DeferredTranslatedResource[+A,R](val resource : ManagedResource[R], val translate : R => A) extends TranslatedResource[A] { self =>
+class DeferredExtractableManagedResource[+A,R](val resource : ManagedResource[R], val translate : R => A) extends ExtractableManagedResource[A] { self =>
 
-  override def map[B](f : A => B) = new DeferredTranslatedResource(resource, translate.andThen(f))
+  override def map[B](f : A => B) = new DeferredExtractableManagedResource(resource, translate.andThen(f))
 
   override def flatMap[B](f : A => B) =  map(f)
 
@@ -147,18 +155,39 @@ class DeferredTranslatedResource[+A,R](val resource : ManagedResource[R], val tr
   override def toTraversable[B](f : A => Iterator[B]) = resource.toTraversable(translate andThen f)
 
   override def equals(that : Any) = that match {
-    case x : DeferredTranslatedResource[A,R] => (x.resource == resource) && (x.translate == translate)
+    case x : DeferredExtractableManagedResource[A,R] => (x.resource == resource) && (x.translate == translate)
     case _ => false
   }
   override def hashCode() : Int = (resource.hashCode << 7) + translate.hashCode + 13
 
-  override def toString = "DefferredTranslatedResource(" + resource + ", " + translate + ")"
+  override def toString = "DeferredExtractableManagedResource(" + resource + ", " + translate + ")"
 }
+
+/**
+ * This class implements ManagedResource methods in terms of acquireFor
+ */
+trait ManagedResourceOperations[+R] extends ManagedResource[R] { self =>
+  //TODO - Will the exception list always have size 1?
+  override def acquireAndGet[B](f : R => B) : B = acquireFor(f).fold( liste => throw liste.head, x => x)
+
+  override def toTraversable[B](f : R => Iterator[B]) : Traversable[B] = new ManagedTraversable[R,B] {
+    val resource = self
+    override protected def iterator(resource : R) = f(resource)
+  }
+
+  override def map[B](f : R => B) : ExtractableManagedResource[B] = new DeferredExtractableManagedResource(this, f)
+
+  //override def flatMap[B](f : H => B) : B  = acquireAndGet(f)
+  override def foreach(f : R => Unit) : Unit = acquireAndGet(f)
+
+  override def and[B](that : ManagedResource[B]) : ManagedResource[(R,B)] = ManagedResource.and(self,that)
+}
+
 
 /**
  * Abstract class implementing most of the managed resource features.
  */
-trait AbstractManagedResource[+R,H] extends ManagedResource[R] { self =>
+trait AbstractManagedResource[+R,H] extends ManagedResource[R] with ManagedResourceOperations[R] { self =>
 
   /** 
    * Opens a given resource, returning a handle to execute against during the "session" of the resource being open.
@@ -188,20 +217,9 @@ trait AbstractManagedResource[+R,H] extends ManagedResource[R] { self =>
     //Combine resulting exceptions as necessary     
     result.left.map[List[Throwable]]( _ :: close.left.toOption.toList)
   }
-
-  //TODO - Will the exception list always have size 1?
-  override def acquireAndGet[B](f : R => B) : B = acquireFor(f).fold( liste => throw liste.head, x => x)
-
-  override def toTraversable[B](f : R => Iterator[B]) : Traversable[B] = new ManagedTraversable[R,B] {
-    val resource = self
-    override protected def iterator(resource : R) = f(resource)
-  }
-
-  override def map[B](f : R => B) : TranslatedResource[B] = new DeferredTranslatedResource(this, f)
-
-  //override def flatMap[B](f : H => B) : B  = acquireAndGet(f)
-  override def foreach(f : R => Unit) : Unit = acquireAndGet(f)
 }
+
+
 
 /**
  * This class is used when a resource is its own handle.
@@ -239,4 +257,17 @@ object ManagedResource {
       override protected val caughtException = nonFatalExceptions
   }
 
+  /**
+   * "ands" two managed resources together.
+   */
+  def and[A,B](r1 : ManagedResource[A], r2 : ManagedResource[B]) : ManagedResource[(A,B)] = new ManagedResource[(A,B)] with ManagedResourceOperations[(A,B)] {
+    override def acquireFor[C](f : ((A,B)) => C) : Either[List[Throwable], C] = {
+      val result = r1.acquireFor({ opened1 =>
+          r2.acquireFor({ opened2 =>
+            f((opened1, opened2))
+          })
+      })
+      result.fold( errors => Left(errors), y => y)      
+    }
+  }
 }
