8b97442f4c2cd5115421c2f5815d574b33d7964d
Joshua.Suereth@gmail.com <jsuereth@jsuereth-laptop.(none)>
2010-04-25 15:35:13 -0400

    Updated SBT build to work against trunk again

--- project/build/ArmProject.scala
+++ project/build/ArmProject.scala
@@ -1,11 +1,19 @@
 import sbt._
 
-class ArmProject(info: ProjectInfo) extends DefaultProject(info) {
-    val scalaToolsSnapshots = "Scala-Tools Maven2 Snapshots Repository" at "http://scala-tools.org/repo-snapshots"
+class ArmProject(info: ProjectInfo) extends DefaultProject(info) with AutoCompilerPlugins {
+  val scalaToolsSnapshots = "Scala-Tools Maven2 Snapshots Repository" at "http://scala-tools.org/repo-snapshots"
+  val jta = "javax.transaction" % "jta" % "1.1" % "provided"
 
-    val jta = "javax.transaction" % "jta" % "1.1" % "provided"
 
-    override def packageDocsJar = defaultJarPath("-javadoc.jar")
-    override def packageSrcJar= defaultJarPath("-sources.jar")
+  override def packageDocsJar = defaultJarPath("-javadoc.jar")
+  override def packageSrcJar= defaultJarPath("-sources.jar")
+
+  val cont = compilerPlugin("org.scala-lang.plugins" % "continuations" % "2.8.0-SNAPSHOT")
+  override def compileOptions = CompileOption("-P:continuations:enable") :: super.compileOptions
+
+
+  val bryanjswift = "Bryan J Swift Repository" at "http://repos.bryanjswift.com/maven2/"
+  val junitInterface = "com.novocode" % "junit-interface" % "0.4.0" % "test"
+  val derby = "org.apache.derby" % "derby" % "10.5.3.0_1" % "test"
 
 }
