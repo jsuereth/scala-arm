40eb65db836b9358ed0f58abef682c1ec5bd1af1
Josh Suereth <joshua.suereth@gmail.com>
2014-03-03 09:21:06 -0500

    Upgrade to sbt 0.13 and cleanup dependencies.

--- project/build.scala
+++ project/build.scala
@@ -14,9 +14,8 @@ object ArmDef extends Build {
     organization := "com.jsuereth",
     name := "scala-arm",
     version := "1.4-SNAPSHOT",
-    scalaVersion := "2.9.2",
-    crossScalaVersions := Seq("2.9.2", "2.10.0"),
-    resolvers += "junit interface repo" at "https://repository.jboss.org/nexus/content/repositories/scala-tools-releases",
+    scalaVersion := "2.10.3",
+    crossScalaVersions := Seq("2.9.3", "2.10.3"),
     resolvers += "java.net repo" at "http://download.java.net/maven/2/",
     libraryDependencies ++= dependencies,
     autoCompilerPlugins := true,
@@ -27,6 +26,7 @@ object ArmDef extends Build {
   def bcSettings: Seq[Setting[_]] = mimaDefaultSettings ++ Seq(
     previousArtifact <<= scalaVersion apply { sv =>
       if(sv startsWith "2.9") Some("com.jsuereth" % "scala-arm_2.9.1" % "1.2")
+      else if(sv startsWith "2.10") Some("com.jsuereth" % "scala-arm_2.10" % "1.2")
       else None
     }
   )
@@ -36,28 +36,20 @@ object ArmDef extends Build {
     publishMavenStyle := true,
     publishArtifact in Test := false,
     // The Nexus repo we're publishing to.
-    publishTo <<= version { (v: String) =>
+    publishTo := {
       val nexus = "https://oss.sonatype.org/"
-      if (v.trim.endsWith("SNAPSHOT")) Some("snapshots" at nexus + "content/repositories/snapshots") 
+      if (version.value.trim.endsWith("SNAPSHOT")) Some("snapshots" at nexus + "content/repositories/snapshots") 
       else                             Some("releases"  at nexus + "service/local/staging/deploy/maven2")
     },
     // Maven central cannot allow other repos.  We're ok here because the artifacts we
     // we use externally are *optional* dependencies.
     pomIncludeRepository := { x => false },
+    
     // Maven central wants some extra metadata to keep things 'clean'.
+    homepage := Some(url("http://jsuereth.com/scala-arm")),
+    licenses += "BSG-Style" -> url("http://www.opensource.org/licenses/bsd-license.php"),
+    scmInfo := Some(ScmInfo(url("http://github.com/jsuereth/scala-arm"), "scm:git@github.com:jsuereth/scala-arm.git")),
     pomExtra := (
-      <url>http://jsuereth.com/scala-arm</url>
-	    <licenses>
-		    <license>
-			    <name>BSD-style</name>
-			    <url>http://www.opensource.org/licenses/bsd-license.php</url>
-			    <distribution>repo</distribution>
-		    </license>
-	    </licenses>
-      <scm>
-        <url>git@github.com:jsuereth/scala-arm.git</url>
-        <connection>scm:git:git@github.com:jsuereth/scala-arm.git</connection>
-      </scm>
       <developers>
         <developer>
           <id>jsuereth</id>
@@ -78,13 +70,11 @@ object ArmDef extends Build {
     )
   )
 
-  def addContinuations = libraryDependencies <<= (scalaVersion, libraryDependencies) apply { (v, d) =>
-    d :+ compilerPlugin("org.scala-lang.plugins" % "continuations" % v)
-  }
+  def addContinuations = libraryDependencies += compilerPlugin("org.scala-lang.plugins" % "continuations" % scalaVersion.value)
 
   def dependencies = Seq(
-    "junit" % "junit" % "4.5" % "test",
-    "com.novocode" % "junit-interface" % "0.7" % "test->default",
+    "junit" % "junit" % "4.10" % "test",
+    "com.novocode" % "junit-interface" % "0.10" % "test",
     "org.apache.derby" % "derby" % "10.5.3.0_1" % "test",
     "javax.transaction" % "jta" % "1.1" % "provided"
   )
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -3,8 +3,8 @@ resolvers += Resolver.url("sbt-plugin-releases", new URL("http://scalasbt.artifa
 resolvers += "jgit-repo" at "http://download.eclipse.org/jgit/maven"
 
 // Note:  Add this to ~/.sbt/plugins/gpg.sbt if you want to sign artifacts.
-//addSbtPlugin("com.typesafe.sbt" % "sbt-pgp" % "0.7")
+//addSbtPlugin("com.typesafe.sbt" % "sbt-pgp" % "0.8")
 
-addSbtPlugin("com.typesafe.sbt" % "sbt-ghpages" % "0.5.0")
+addSbtPlugin("com.typesafe.sbt" % "sbt-ghpages" % "0.5.2")
 
-addSbtPlugin("com.typesafe" % "sbt-mima-plugin" % "0.1.3")
+addSbtPlugin("com.typesafe" % "sbt-mima-plugin" % "0.1.6")
